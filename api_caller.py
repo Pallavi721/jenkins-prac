9bd1beb3863bc817329f397d61a99455cf2c6860
Vusthulamuri-Pallavi-xor <Vusthulamuri.Pallavi@Xoriant.com>
2026-06-04 13:37:57 +0530

    webhook test

---
+++ api_caller.py
@@ -0,0 +1,19 @@
+import requests
+import json
+from datetime import datetime
+
+url = "https://jsonplaceholder.typicode.com/users"
+
+response = requests.get(url)
+
+users = response.json()
+
+with open("users.json", "w") as file:
+    json.dump(users, file, indent=4)
+
+print(f"Retrieved {len(users)} users")
+
+with open("logs/app.log", "a") as log:
+    log.write(
+        f"{datetime.now()} SUCCESS Retrieved {len(users)} users\n"
+    )
---
+++ report_generator.py
@@ -0,0 +1,9 @@
+import json
+
+with open("users.json") as file:
+    users = json.load(file)
+
+with open("reports/report.txt", "w") as report:
+    report.write(f"Total Users: {len(users)}\n")
+
+print("Report generated")
